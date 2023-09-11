57081f89ea3b282edad57cdbec21b0c39999281a
katha922 <kathasaha922@gmail.com>
2023-09-11 23:16:32 +0600

    11-09-23

---
+++ day1/script.js
@@ -0,0 +1,19 @@
+/* const notesContainer=document.querySelector(".notes-container");
+const createBtn=document.querySelector(".btn");
+let notes=document.querySelectorAll(".input-box");
+createBtn.addEventListener("click",()=>{
+    let inputBox=document.createElement("p");
+    let button=document.createElement("img");
+    inputBox.className="input-box";
+    inputBox.setAttribute("contenteditable","true");
+    img.src="delete.png";
+    notesContainer.appendChild(inputBox).appendChild(img);
+}) */
+/* document.getElementById("createButton").addEventListener("click", function () {
+    const codeHTML = `<div class="notes-container">
+        <p contenteditable="true" class="input-box">
+            <img src="delete.png">
+        </p>
+    </div>`;
+    document.getElementById("notes-container").innerHTML = codeHTML;
+}); */
